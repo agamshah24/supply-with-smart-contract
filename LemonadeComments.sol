e90f8408feb11c73e05227eb52d9057bbe495511
agamshah24 <agam.udemy@gmail.com>
2020-09-14 09:57:51 +0530

    Defining smart contract structure

---
+++ LemonadeComments.sol
@@ -0,0 +1,35 @@
+// Define Supply chain contract "LemonadeContract"
+
+// Variable: 'Owner'
+
+// Variable: 'skuCount'
+
+// State: For sale
+
+// Struct: 'Item' with the following fields: name, sku, price, state, seller, buyer
+
+// Mapping: Assign 'Item' a SKU
+
+// Event ForSale
+
+// Event Sold
+
+// Modifier: Only Owner
+
+// Modifier: Verify Caller
+
+// Modifier: Paid Enough
+
+// Modifier: For Sale
+
+// Modifier: Sold
+
+// Modifier: Only Owner
+
+// Function: Constructor to set some initial values
+
+// Function: Add Item
+
+// Function: Buy Item
+
+// Function: Fetch Item
