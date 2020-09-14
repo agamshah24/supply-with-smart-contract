e0c53c0a0f4d179c9385933e77a584c57e90ca12
agamshah24 <agam.udemy@gmail.com>
2020-09-14 10:12:55 +0530

    Setup Smart contract with Variables, Mapping and structs

--- LemonadeComments.sol
+++ LemonadeComments.sol
@@ -1,16 +1,33 @@
+pragma solidity ^0.6.9;
+
 // Define Supply chain contract "LemonadeContract"
+contract LemonadeStand {
 
-// Variable: 'Owner'
+    // Variable: 'Owner'
+    address owner;
 
-// Variable: 'skuCount'
+    // Variable: 'skuCount'
+    uint skuCount;
 
-// State: For sale
+    // Enum: 'State' with values 'ForSale' (State: For sale)
+    enum State {ForSale, Sold}
 
-// Struct: 'Item' with the following fields: name, sku, price, state, seller, buyer
+    // Struct: 'Item' with the following fields: name, sku, price, state, seller, buyer
+    struct Item {
+        string name;
+        uint sku;
+        uint price; // price is not going to be negative so we used uint.
+        State state;
+        address seller;
+        address buyer;
+    }
 
-// Mapping: Assign 'Item' a SKU
+    // Mapping: Assign 'Item' a SKU
+    // Define mapping 'items' that maps the SKU (a number) to an Item.
+    mapping (uint => Item) items;
 
-// Event ForSale
+    // Event ForSale
+
 
 // Event Sold
 
@@ -33,3 +50,5 @@
 // Function: Buy Item
 
 // Function: Fetch Item
+
+}
